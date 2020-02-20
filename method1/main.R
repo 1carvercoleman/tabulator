63c521cd4b23fb75ec3251192870e3b9c1e1053d
Carver Coleman <58525599+1carvercoleman@users.noreply.github.com>
2020-02-20 09:44:42 -0700

    Update main.R

--- method1/main.R
+++ method1/main.R
@@ -6,11 +6,11 @@ library(stringr)
 library(Rcpp)
 source('tabulator.R')
 
-cut_file <- ('tester.jpg')
+cut_file <- ('example_page_county_codes_1.png')
 valid_chars <- tesseract(language = "eng", options = list(tessedit_char_whitelist = " 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ."))
 text <- tesseract::ocr_data(cut_file, engine = valid_chars)
 img <- magick::image_read(cut_file)
 
 final_data <- tabulator(text, img)
 
-write.csv(final_data, "test.csv", row.names = FALSE)
+write.csv(final_data, "example_codes.csv", row.names = FALSE)
